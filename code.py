634ad790f6451a70b55e4f5d770915bf44011eaa
Lingrui <lingrui@umich.edu>
2022-08-24 10:06:19 -0400

    add comments

--- code.py
+++ code.py
@@ -1,16 +1,29 @@
-def function1(a):
-    b = dict()
-    for c in a:
-        if c not in b:
-            b[c] = 1
+def count_number(string):
+    '''
+    count the number of each base in the string as input
+    return the count in dictionary
+    parameters:
+        -string: input sequencing? 
+        -base_dict: output a dictionary which the key is the base name 
+                    and the value is the number of occurrance
+    '''
+    base_dict = dict()
+    for s in string:
+        if s not in base_dict:
+            base_dict[s] = 1
         else:
-            b[c] += 1
-    return b
+            base_dict[s] += 1
+    return base_dict
 
-def function2(a):
+def cal_freq(base_dict):
+    '''
+    calculate the frequecy of each base 
+    parameter:
+
+    '''
     print('freqs')
-    total = float(sum([a[b] for b in a.keys()]))
-    for b in a.keys():
-        print(b + ':' + str(a[b]/total))
+    total = float(sum([base_dict[b] for b in base_dict.keys()]))
+    for b in base_dict.keys():
+        print(b + ':' + str(base_dict[b]/total))
 
-function2(function1('ATCTGACGCGCGCCGC'))
+cal_freq(count_number('ATCTGACGCGCGCCGC'))
